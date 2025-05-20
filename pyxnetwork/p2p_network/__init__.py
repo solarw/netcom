c0944908e5c02e2ebc8f864b53c6b04e1323fb5a
Yuri (solarw) Turchenkov <solarw.mail@gmail.com>
2025-05-20 14:45:29 +0300

    python fixes

--- pyxnetwork/p2p_network/__init__.py
+++ pyxnetwork/p2p_network/__init__.py
@@ -1,4 +1,4 @@
-from p2p_network_py import Node, PeerId, KeyPair, generate_keypair, peer_id_from_keypair, ProofOfRepresentation
+from pyxnetwork import Node, PeerId, KeyPair, generate_keypair, peer_id_from_keypair, ProofOfRepresentation
 
 __version__ = "0.1.0"
 
--- pyxnetwork/src/lib.rs
+++ pyxnetwork/src/lib.rs
@@ -3,3 +3,35 @@ pub mod events;
 pub mod node;
 pub mod types;
 pub mod xstream;
+
+use pyo3::prelude::*;
+
+
+
+#[pymodule]
+fn pyxnetwork(py: Python, m: &PyModule) -> PyResult<()> {
+    // Current best way to initialize the tokio runtime for PyO3
+    let _ = pyo3_asyncio::tokio::future_into_py(py, async {
+        // Just a dummy future to trigger initialization
+        Ok(())
+    });
+
+    // Register the Node class
+    m.add_class::<node::Node>()?;
+
+    // Register the XStream class
+    m.add_class::<xstream::XStream>()?;
+
+    // Register utility types
+    m.add_class::<types::PeerId>()?;
+    m.add_class::<types::KeyPair>()?;
+    m.add_class::<types::ProofOfRepresentation>()?;
+    m.add_class::<types::PorUtils>()?;
+
+    // Add utility functions
+    m.add_function(wrap_pyfunction!(types::generate_keypair, m)?)?;
+    m.add_function(wrap_pyfunction!(types::peer_id_from_keypair, m)?)?;
+    m.add_function(wrap_pyfunction!(types::generate_por, m)?)?;
+
+    Ok(())
+}
